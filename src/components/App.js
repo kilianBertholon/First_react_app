d918e633b32ea0c4c71531d37e5c51f0613da95e
kilianBertholon <kilian.bertholon02@gmail.com>
2023-07-19 17:29:17 +0200

    Ajout de component supplémentaires

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,29 @@
-import Banner from "./Banner"
-import Cart from "./Cart"
-import ShoppingList from "./ShoppingList";
-
+import Banner from './Banner'
+import logo from '../assets/logo.png'
+// import Cart from './Cart'
+import ShoppingList from './ShoppingList'
 
 function App() {
-  return <div>
-    <Banner />
-    <Cart />
-    <ShoppingList />
-    </div>
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+			{/* <Cart /> */}
+			<ShoppingList />
 
+      <form onSubmit={handleSubmit}>
+        <input type='text' name='my_input' defaultValue='tapez votre texte' />
+        <button type='submit'>Entrez</button>
+      </form>
+		</div>
+	)
 }
 
-export default App;
+export default App
+
+function handleSubmit(e) {
+  e.preventDefault()
+  alert(e.target['my_input'].value)
+}
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,15 +1,7 @@
 import '../styles/Banner.css'
-import logo from '../assets/logo.png'
 
-function Banner() {
-    const title = 'Green Spirit'
-    return(
-        <div className="banner-info">
-            <img src={logo} alt='Logo génépi' className='logo-banner' />
-            <h1 className='banner-titre'>{title}</h1> 
-    </div>
-    )
-
+function Banner({ children }) {
+	return <div className='lmj-banner'>{children}</div>
 }
 
 export default Banner
